7c55eb79aaf43382f2c0f082d4823cbc65b7fe4f
Rubel Hossain99 <rubel820746@gmail.com>
2022-11-23 21:48:14 +0600

    I will install and Config google firebase Auth

---
+++ src/FireBase/FireBase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAAk3R-0Xk7SGALT051x2YQd-50iAb-iM",
+  authDomain: "jhbook-8c684.firebaseapp.com",
+  projectId: "jhbook-8c684",
+  storageBucket: "jhbook-8c684.appspot.com",
+  messagingSenderId: "878203701375",
+  appId: "1:878203701375:web:408dd325ec00ef205a174a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
